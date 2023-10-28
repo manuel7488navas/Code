01e5d5be0c32f3aedab269aeb6d7cbe3534f0813
Manu <nele7488@gmail.comcls>
2023-10-28 12:54:16 +0200

    Ejercicio Rock Paper Scissors realizado correctamente.

--- ODIN/JavaScript/RockPaperScissors/scripts.js
+++ ODIN/JavaScript/RockPaperScissors/scripts.js
@@ -9,38 +9,37 @@ function getComputerChoice(){
 function game(playerSelection, computerSelection){
 
  computerSelection = getComputerChoice();
- playerSelection = prompt("Choose your fighter!!!");
-    
+ playerSelection = prompt("Choose your fighter!!!").toLowerCase();
+
     if (playerSelection === "rock" && computerSelection === "scissors"){
-        return alert("You win!!!");
+        return alert(`You win! ${playerSelection} beats ${computerSelection}`);
     }else if (playerSelection === "rock" && computerSelection === "paper"){
-        return  alert("You loose.");
+        return  alert(`You lose. ${computerSelection} beats ${playerSelection}`);
     }else if (playerSelection === "rock" && computerSelection === "rock"){
         return  alert("It´s a tie!!!.");
     }else if (playerSelection === "paper" && computerSelection === "scissors"){
-        return  alert("You loose.");
+        return  alert(`You lose. ${computerSelection} beats ${playerSelection}`);
     }else if (playerSelection === "paper" && computerSelection === "paper"){
         return  alert("It´s a tie!!!.");
     }else if (playerSelection === "paper" && computerSelection === "rock"){
-        return  alert("You win!!!");
+        return  alert(`You win! ${playerSelection} beats ${computerSelection}`);
     }else if (playerSelection === "scissors" && computerSelection === "scissors"){
         return  alert("It´s a tie!!!.");
     }else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return  alert("You win!!!");
+        return  alert(`You win! ${playerSelection} beats ${computerSelection}`);
     }else if (playerSelection === "scissors" && computerSelection === "rock"){
-        return  alert("You loose.");
+        return  alert(`You lose. ${computerSelection} beats ${playerSelection}`);
     }else if (playerSelection === ""){
         return  alert("SAY SOMETHING.");
     }
 
 
 
-}
-for (let i = 5; i >=0; i--){
-    prompt(game());
-}
 
-
+}
+for (let i = 100; i >=0; i--){
+     game();
+}
 
 
 
