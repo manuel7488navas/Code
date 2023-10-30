06a0081b9c8d20ba0346adbffc19dc1036c892e0
Manu <nele7488@gmail.comcls>
2023-10-30 11:50:30 +0100

    Realizados ejercicios correctamente 01,02,03,04,05 de javascript-exercises TDD.

--- ODIN/JavaScript/javascript-exercises/05_sumAll/sumAll.js
+++ ODIN/JavaScript/javascript-exercises/05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function sumar(a, b) {
+    let total = "";
+    if (a<0 || b<0){
+        return 'ERROR';
+    }else if ( typeof(a) !== "number" || typeof(b) !== "number" ){
+        return 'ERROR';
+    }else if (a<b){
+       for (let i = a; i<=b; i++){
+               total += i;
 
-};
+               for (let j = a; j<=total.length;j++){
+                   total = j++;
+               }  
+    
+           }
+           return total;   
+    }else if (a>b){
+        for (let i = a; i>=b; i--){
+            total += i;
+            for (let j = a; j>=total.length;j++){
+                total = j++;
+            }
+        }
+        return total;
+    }
+}
+
+
+          
 
 // Do not edit below this line
 module.exports = sumAll;
