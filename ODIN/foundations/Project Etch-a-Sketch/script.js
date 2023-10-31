94e7b3ceeab20610aae1e8194b8c58c6cbeb342a
Manu <nele7488@gmail.comcls>
2023-10-31 19:58:12 +0100

    Desarrollando Project Etch-a-Sketch 31/10/2023-noche.

--- ODIN/foundations/Project Etch-a-Sketch/script.js
+++ ODIN/foundations/Project Etch-a-Sketch/script.js
@@ -1,33 +1,54 @@
 const main = document.createElement("main");
 document.body.appendChild(main);
-const container = document.createElement("container");
-main.appendChild(container);
 const grid = document.createElement("div");
-container.appendChild(grid);
+main.appendChild(grid);
 document.querySelector("div").className = "grid";
+const button = document.createElement("button");
+main.appendChild(button);
+button.className = "button"; 
+const buttonText = document.createElement("div");
+button.appendChild(buttonText);
+buttonText.className = "buttonText";
+buttonText.textContent = "Choose Grid Size";
 
 
-for (let i = 0; i<16; i++){
-    for (let j = 0; j<16; j++){
-const cell = document.createElement("div");
-grid.appendChild(cell);
-cell.className = "cell"; 
+button.addEventListener('click',()=>{
+  const promptValue = prompt("Grid size.");
+  function createGrid(cols){
+    let rows = cols;
+    for (let i = 0; i<rows; i++){
+        for (let j = 0; j<cols; j++){
+        const cell = document.createElement("div");
+        grid.appendChild(cell);
+        cell.className = "cell";
+         
+        cell.addEventListener('mouseover', () => {
 
-cell.addEventListener('mouseover', () => {
+          cell.style.backgroundColor = "rgb(28, 69, 113)";
+      
+        }); 
+        }
+      
+    }  
+
+  }
+  
+  createGrid(promptValue);
+})
+
+
+
+/*cell.addEventListener('mouseover', () => {
 
     cell.style.backgroundColor = "rgb(28, 69, 113)";
 
   });
 
-  cell.addEventListener('mouseout', () => {
+  /*cell.addEventListener('mouseout', () => {
 
     cell.style.backgroundColor = '';
-  });
-}
-}
+  });*/
 
-const button = document.createElement("button");
-main.appendChild(button);
-button.className = "button"; 
+
 
 
