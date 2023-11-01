ec86a570fd3fb063c87795e482430d242ae09524
Manu <nele7488@gmail.comcls>
2023-11-01 19:54:04 +0100

    Ejercicio 09_palindromes realizado correctamente.

--- ODIN/JavaScript/javascript-exercises/09_palindromes/palindromes.js
+++ ODIN/JavaScript/javascript-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let str2 = "";
+    let str3 ="";
+    let str4 ="";
+        
+    for (let i = str.length-1; i>=0;i--){
+        str2 += str[i];
+        str3 = str2.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s0-9]+/g, '').toLowerCase();
+        str4 = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s0-9]+/g, '').toLowerCase();
+    }
+ 
+    if (str3 === str4){
+        return  true;
+    }
+        return false;
+    }
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
