8ac5fd9ca9099c830d8fc35deddc4cf602793142
Manu <nele7488@gmail.comcls>
2023-11-02 09:35:20 +0100

    Ejercicio 10_fibonacci realizado correctamente.

--- ODIN/JavaScript/javascript-exercises/10_fibonacci/fibonacci.js
+++ ODIN/JavaScript/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function fibo(num) {
+    if (num<0){
+        return "OOPS";
+    }else if(num == 0){
+        return 0;
+      } else if (num == 1){
+        return 1;
+      }else{
+        return (fibo(num - 1)+fibo(num - 2));
+      }
 };
 
 // Do not edit below this line
