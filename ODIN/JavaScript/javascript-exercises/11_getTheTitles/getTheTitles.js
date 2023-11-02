976354329ac6e9c5b6eb132a5965ecd4df2326e9
Manu <nele7488@gmail.comcls>
2023-11-02 10:36:35 +0100

    Ejercicio 11_getTheTitles realizado correctamente.

--- ODIN/JavaScript/javascript-exercises/11_getTheTitles/getTheTitles.js
+++ ODIN/JavaScript/javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let result = [];
+    result = books.map(function(book){
+      return book.title;
+    })
+          return result;
+}
 
-};
 
 // Do not edit below this line
 module.exports = getTheTitles;
