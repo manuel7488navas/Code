4043757e8f9973c697c798332d30d9dec23cceef
Manu <nele7488@gmail.comcls>
2023-11-02 12:50:01 +0100

    Ejercicio 12_findTheOldest realizado correctamente.

--- ODIN/JavaScript/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ ODIN/JavaScript/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(
+        person.yearOfBirth,
+        person.yearOfDeath
+      );
+      return oldestAge < currentAge ? person : oldest;
+    });
+  };
+  
+  const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  
+  module.exports = findTheOldest;
 
-};
 
-// Do not edit below this line
-module.exports = findTheOldest;
+
+  
