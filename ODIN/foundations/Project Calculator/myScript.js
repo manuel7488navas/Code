201d51d3b43f8ae56e294eebfa8b420d39eac43e
Manu <nele7488@gmail.comcls>
2023-11-03 20:00:48 +0100

    Developing Project Calculator 20:00

--- ODIN/foundations/Project Calculator/myScript.js
+++ ODIN/foundations/Project Calculator/myScript.js
@@ -1,124 +1,48 @@
-let a;
-let b;
-let operator;
+
+document.querySelector(".calculator").style.border = "solid black";
+document.querySelector(".screen").style.border = "solid black";
 
 function add(a,b){
-    return a + b;
-}
-
+    return a+b;
+};
 function subtract(a,b){
-    return a - b;
-}
-
+    return a-b;
+};
 function multiply(a,b){
-    return a*b;
-}
- 
+    return a*b
+};
 function divide(a,b){
     if (b === 0){
-        return "you can´t divide by 0";
+        return "ERROR"
     }
         return a/b;
-}
+};
 
 
-
-function operate(operator,a,b){
+function operate(operator, a, b){
     switch (operator){
-        case 'add':
-            return add(a, b);
-        case 'subtract':
-            return subtract(a, b);
-        case 'multiply':
-            return multiply(a, b);
-        case 'divide':
-            return divide(a, b);
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
         default:
-            return "Wrong operator";
-    }
-}
+            return "ERROR";
+    }   
+};
 
+let firstNum = "";
+let operator = "";
+let secondNum = "";
 
-console.log(operate('multiply',2,3))
-
-
-
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('.button');
-
-let value = '';
-
-
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    value = button.id;
-    display.textContent = value;
-
-  });
-  
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button)=>{
+    button.addEventListener('click', (event)=>{
+        const digit =event.target.textContent;
+        handleDigitClick(digit);
+    });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const main = document.createElement("main");
-document.body.appendChild(main);
-
-const calculator = document.createElement("div");
-main.appendChild(calculator);
-calculator.className ="calculator";
-
-const screen = document.createElement("div");
-calculator.appendChild(screen);
-screen.className = "screen";
-
-const buttonGrid = document.createElement("div");
-calculator.appendChild(buttonGrid);
-buttonGrid.className = "buttonGrid";
-
-const button = document.createElement("button");
-buttonGrid.appendChild(button);
-button.className = "CE";
-button.id ="CE";
-CE.textContent = "CE";
-
-const button1 = document.createElement("button");
-buttonGrid.appendChild(button1);
-button1.className = "C";
-button1.id ="C";
-C.textContent = "C";
-
-
-
-function createButton(button){
-    let cols = 20;
-    buttonGrid.removeChild(button);
-    for (let i = 0; i<cols; i++){
-        const button = document.createElement("button");
-        buttonGrid.appendChild(button);
-        button.className = `${"abcdefghijklmnopqrst"[i]}`;
-        //button.textContent = `${[i]}`;
-       
-        }
-    }
-
-createButton(button);
-
-//document.querySelector(a).textContent = "CE"*/
