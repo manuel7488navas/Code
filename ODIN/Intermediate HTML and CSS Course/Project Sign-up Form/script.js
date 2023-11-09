d12e97a6c311508440a38908c9aa8e9216ebd598
Manu <nele7488@gmail.comcls>
2023-11-09 20:02:10 +0100

    Project: Sign-up Form done.

--- ODIN/Intermediate HTML and CSS Course/Project Sign-up Form/script.js
+++ ODIN/Intermediate HTML and CSS Course/Project Sign-up Form/script.js
@@ -1,12 +1,30 @@
 const form = document.querySelector("form");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword")
-const error = document.querySelector("#password + span.error");
 
-function verifyPassword(){
-    let passwordValue = document.getElementById("password").value;
-    if(passwordValue === " "){
-        document.getElementsById("error").innerHTML = "Must enter a valid password, please."
-        return false;  
+
+
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    let submitButton = document.getElementById('submitButton');
+    let passwordInput = document.getElementById('password');
+    let confirmPasswordInput = document.getElementById('confirmPassword');
+    let error = document.getElementById("error");
+    
+
+    submitButton.addEventListener('click', ()=> {
+        
+        let passwordValue = passwordInput.value;
+        let confirmPasswordValue = confirmPasswordInput.value;
+    if (passwordValue !== confirmPasswordValue){
+        error.textContent = "Error. Passwords don't match!";
+       
+    }else{
+        error.textContent ="";
     }
-}
+    
+    });
+         
+});
