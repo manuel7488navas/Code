8cf244ac54e6ef3af6229dc24be896e104faf6c0
manu <nele7488@gmail.com>
2023-11-16 13:02:04 +0100

    Developing Project Library.

---
+++ ODIN/JavaScript/Project Library/script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(name, author, pages, year) {
+  // the constructor...
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.year = year;
+
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  for (let i=0;i<myLibrary.length;i++){
+  }
+}
