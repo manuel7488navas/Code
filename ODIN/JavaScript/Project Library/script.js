741191107c07560d30e6e65b82ea50b8c7d63167
manu <nele7488@gmail.com>
2023-11-17 14:51:11 +0100

    Developing Project Library v1.4

--- ODIN/JavaScript/Project Library/script.js
+++ ODIN/JavaScript/Project Library/script.js
@@ -1,26 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Open pop-up
 document.getElementById("newBook").addEventListener("click",function(){
   document.querySelector(".pop-up").style.display= "flex";
@@ -33,11 +11,12 @@ document.getElementById("add").addEventListener("click",function(){
 
 let myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, pages, year) {
   // the constructor...
   this.title = title;
   this.author = author;
-
+  this.pages = pages;
+  this.year = year;
 
 }
 
@@ -46,16 +25,24 @@ function addBookToLibrary() {
   // do stuff here
   const title = document.getElementById("title");
   title.textContent = document.getElementById("title").value;
+
   const author = document.getElementById("author");
   author.textContent = document.getElementById("author").value;
-  const btn = document.getElementById("add"); 
-  btn.onclick = ()=>{
-  const book = new Book(title.value, author.value);
-  myLibrary.push(book);  
 
+  const pages = document.getElementById("pages");
+  pages.textContent = document.getElementById("pages").value;
+
+  const year = document.getElementById("year");
+  year.textContent = document.getElementById("year").value;
+
+  const btn = document.getElementById("add"); 
+
+  btn.onclick = ()=>{
+  const book = new Book(title.value, author.value, pages.value, year.value);
+  myLibrary.push(book);  
+  return myLibrary;
   }
  
-
  };
 
 
