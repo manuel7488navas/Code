09597cefc230fc1541eba026ee11095ce6863562
manu <nele7488@gmail.com>
2023-11-17 20:02:12 +0100

    Developing Project Library v1.5

--- ODIN/JavaScript/Project Library/script.js
+++ ODIN/JavaScript/Project Library/script.js
@@ -23,29 +23,58 @@ function Book(title, author, pages, year) {
  
 function addBookToLibrary() {
   // do stuff here
-  const title = document.getElementById("title");
+  const container = document.getElementById("container");
+  const wrapper = document.getElementById("wrapper");
+  container.appendChild(wrapper);
+
+  let title = document.getElementById("title");
   title.textContent = document.getElementById("title").value;
 
-  const author = document.getElementById("author");
+  let author = document.getElementById("author");
   author.textContent = document.getElementById("author").value;
 
-  const pages = document.getElementById("pages");
+  let pages = document.getElementById("pages");
   pages.textContent = document.getElementById("pages").value;
 
-  const year = document.getElementById("year");
+  let year = document.getElementById("year");
   year.textContent = document.getElementById("year").value;
 
   const btn = document.getElementById("add"); 
 
   btn.onclick = ()=>{
   const book = new Book(title.value, author.value, pages.value, year.value);
-  myLibrary.push(book);  
-  return myLibrary;
+  myLibrary.push(book); 
+  
+
+  const card =document.createElement("div");
+  card.id ="card";
+  wrapper.appendChild(card);
+
+  title = document.createElement("h1");
+  title.id = "title";
+  card.appendChild(title);
+  title.textContent = document.getElementById("title").value;
+
+  author = document.createElement("h2");
+  author.id = "author";
+  card.appendChild(author);
+  author.textContent = document.getElementById("author").value;
+
+  pages = document.createElement("h3");
+  pages.id = "pages";
+  card.appendChild(pages);
+  pages.textContent =document.getElementById("pages").value +" Pages";
+
+  year = document.createElement("h3");
+  year.id = "year";
+  card.appendChild(year);
+  year.textContent = "Year: "+document.getElementById("year").value;
+
   }
+
+
  
  };
 
-
 addBookToLibrary();
 
-console.log(myLibrary);
