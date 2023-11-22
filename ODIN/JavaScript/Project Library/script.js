91da16df36645e42b11e6c9b43be82f9130cb320
manu <nele7488@gmail.com>
2023-11-22 12:55:22 +0100

    Developing Project Library v.4.0 FINISHED

--- ODIN/JavaScript/Project Library/script.js
+++ ODIN/JavaScript/Project Library/script.js
@@ -17,12 +17,14 @@ document.getElementById("close").addEventListener("click", () => {
 //My library...
 let myLibrary = [];
   // the constructor...
-function Book(title, author, pages, year) {
+function Book(title, author, pages, year, read) {
 
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.year = year;
+  this.read = read;
+  this.cardId="";
 
 }
 
@@ -35,20 +37,17 @@ function addBookToLibrary() {
   container.appendChild(wrapper);
 
   const title = document.getElementById("title");
-  title.textContent = document.getElementById("title").value;
-
   const author = document.getElementById("author");
-  author.textContent = document.getElementById("author").value;
-
   const pages = document.getElementById("pages");
-  pages.textContent = document.getElementById("pages").value;
-
   const year = document.getElementById("year");
-  year.textContent = document.getElementById("year").value;
+  const read = document.getElementById("status");
 
-  const buttonAdd = document.getElementById("add"); 
+  const card = document.getElementById("wrapper").lastChild;
+  const cardId = card.id;
 
-  const book = new Book(title.value, author.value, pages.value, year.value);
+
+  const book = new Book(title.value, author.value, pages.value, year.value, read);
+  book.cardId = cardId;
   myLibrary.push(book);
   console.log(myLibrary);
   resetInputFields();
@@ -62,11 +61,12 @@ function addBookToLibrary() {
   function createCard(){
     const wrapper = document.getElementById("wrapper");
 
-    
-      const card =document.createElement("div");
-      card.id ="card";
-      card.setAttribute("index", myLibrary.length); 
-      wrapper.appendChild(card);
+    const card = document.createElement("div");
+    const cardId = "card_" + Date.now();
+    const cardIndex = myLibrary.length;
+    card.classList.add("card");
+    card.id = cardId;
+    wrapper.appendChild(card);
 
       const title = document.createElement("h1");
       title.id = "title";
@@ -97,6 +97,7 @@ function addBookToLibrary() {
       toggle.appendChild(read);
 
       const input = document.createElement("input");
+      input.id="status_" + cardId;
       input.type="checkbox";
       input.checked = false;
       toggle.appendChild(input);
@@ -106,22 +107,46 @@ function addBookToLibrary() {
       toggle.appendChild(span);
 
 
+      input.addEventListener("change", function() {
+        const cardId = card.id;
+        const book = myLibrary.find((book) => book.cardId === cardId);
+      
+        if (book) {
+          book.read = this.checked; // Update read status
+          console.log(myLibrary);
+        }
+      });
+      
 
+
+
+
+
+
+      //Delete button of each card
       const deleteButton = document.createElement("button");
       deleteButton.textContent="X";
       deleteButton.id ="deleteButton";
       card.appendChild(deleteButton);
+
+
       deleteButton.addEventListener("click", function(){
+       
+        const cardId = card.id;
+
         wrapper.removeChild(card); 
 
         // Retrieve the index from the data attribute
-        const cardIndex = parseInt(card.getAttribute("index"), 10);
+           const bookIndex = myLibrary.findIndex((book) => book.cardId === cardId);
+
+          
+
 
         // Remove the corresponding book from myLibrary
-        if (!isNaN(cardIndex)) {
-          myLibrary.splice(cardIndex, 1);
+        if (bookIndex !== -1) {
+          myLibrary.splice(bookIndex, 1);
           console.log(myLibrary);
-      }
+        }
       });
       
       };
